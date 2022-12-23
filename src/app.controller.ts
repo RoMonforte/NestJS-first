a4180e352d6e8807a13b8b3fb11f4168a4a5dcd6
Rodrigo Monforte <rodrigomonforte94@gmail.com>
2022-12-23 03:15:52 -0600

    add first controllers

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,19 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('nuevo')
+  newEndpoint() {
+    return 'yo soy nuevo';
+  }
+
+  @Get('products/:productId')
+  getProduct(@Param('productId') productId: string) {
+    return `product ${productId}`;
+  }
+  @Get('categories/:id/products/:productId')
+  getCategory(@Param('productId') productId: string, @Param('id') id: string) {
+    return `product ${productId} and category ${id}`;
   }
 }
