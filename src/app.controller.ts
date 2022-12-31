2b8d5ae557f6bdc7fafddcdde5a47eff79a7e84d
Rodrigo Monforte <rodrigomonforte94@gmail.com>
2022-12-31 03:05:49 -0600

    single responsability

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -13,21 +13,5 @@ export class AppController {
   @Get('nuevo')
   newEndpoint() {
     return 'yo soy nuevo';
-  }
-  @Get('products')
-  getProducts(
-    @Query('limit') limit = 100,
-    @Query('offset') offset = 0,
-    @Query('brand') brand: string,
-  ) {
-    return `products limit=>${limit} offset=> ${offset} brand=> ${brand}`;
-  }
-  @Get('products/:productId')
-  getProduct(@Param('productId') productId: string) {
-    return `product ${productId}`;
-  }
-  @Get('categories/:id/products/:productId')
-  getCategory(@Param('productId') productId: string, @Param('id') id: string) {
-    return `product ${productId} and category ${id}`;
   }
 }
