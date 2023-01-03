e15bfb89344ec3ab9e9a0951bef85daad685f706
Rodrigo Monforte <rodrigomonforte94@gmail.com>
2023-01-03 02:35:55 -0600

    add services of products

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,7 +1,6 @@
 import {
   Controller,
   Get,
-  Query,
   Param,
   Post,
   Body,
@@ -9,45 +8,31 @@ import {
   Delete,
   HttpCode,
   HttpStatus,
-  Res,
 } from '@nestjs/common';
 
-import { Response } from 'express';
+import { ProductsService } from 'src/services/products.service';
 
 @Controller('products')
 export class ProductsController {
+  constructor(private productsService: ProductsService) {}
   @Get()
-  getProducts(
-    @Query('limit') limit = 100,
-    @Query('offset') offset = 0,
-    @Query('brand') brand: string,
-  ) {
-    return {
-      message: `products limit=>${limit} offset=> ${offset} brand=> ${brand}`,
-    };
+  getProducts() {
+    return this.productsService.findAll();
   }
   @Get(':productId')
   @HttpCode(HttpStatus.ACCEPTED)
-  getProduct(@Res() response: Response, @Param('productId') productId: string) {
-    response.status(200).send({
-      message: `product ${productId}`,
-    });
+  getProduct(@Param('productId') productId: string) {
+    return this.productsService.findOne(+productId);
   }
 
   @Post()
   create(@Body() payload: any) {
-    return {
-      message: 'accion de crear',
-      payload,
-    };
+    this.productsService.create(payload);
   }
 
   @Put(':id')
-  update(@Param('id') id: number, @Body() payload: any) {
-    return {
-      id,
-      payload,
-    };
+  update(@Param('id') id: string, @Body() payload: any) {
+    return this.productsService.update(+id, payload);
   }
 
   @Delete(':id')
