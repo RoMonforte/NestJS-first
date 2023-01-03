6840e53d9f8f24e1676669ea270ddc13721502a8
Rodrigo Monforte <rodrigomonforte94@gmail.com>
2023-01-03 02:48:01 -0600

    first errors with nest

--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 
 import { Product } from './../entities/product.entity';
 @Injectable()
@@ -20,7 +20,11 @@ export class ProductsService {
   }
 
   findOne(id: number) {
-    return this.products.find((item) => item.id === id);
+    const product = this.products.find((item) => item.id === id);
+    if (!product) {
+      throw new NotFoundException(`Product with id #${id} not found!`);
+    }
+    return product;
   }
 
   create(payload: any) {
@@ -43,5 +47,13 @@ export class ProductsService {
       return this.products[index];
     }
     return null;
+  }
+  remove(id: number) {
+    const index = this.products.findIndex((item) => item.id === id);
+    if (index === -1) {
+      throw new NotFoundException(`Product with id #${id} not found!`);
+    }
+    this.products.splice(index, 1);
+    return true;
   }
 }
