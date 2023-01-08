fb4eeaf28dc026e3b54a9aed8b109765cc54a7c2
Rodrigo Monforte <rodrigomonforte94@gmail.com>
2023-01-08 05:15:42 -0600

    connection to db in postgres in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import * as Joi from 'joi';
+import { Client } from 'pg';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -9,6 +10,20 @@ import { ProductsModule } from './products/products.module';
 import { DatabaseModule } from './database/database.module';
 import { enviroments } from '../enviroments';
 import config from './config';
+
+const client = new Client({
+  user: 'RmC',
+  host: 'localhost',
+  database: 'postgres',
+  password: 'admin123',
+  port: 5432
+})
+
+client.connect();
+client.query('SELECT * FROM tasks', (err, res) => {
+  console.error(err);
+  console.log(res.rows);
+});
 
 
 
