2b63402b7eacbdcab46e4f192511631668457b31
Rodrigo Monforte <rodrigomonforte94@gmail.com>
2023-01-13 03:26:22 -0600

    add query filter by price

--- src/products/services/products.service.ts
+++ src/products/services/products.service.ts
@@ -4,7 +4,7 @@ import {
   NotAcceptableException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Repository, Between , FindConditions} from 'typeorm';
 
 import {
   CreateProductDto,
@@ -28,9 +28,15 @@ export class ProductsService {
 
   findAll(params?: FilterProductsDto) {
     if (params) {
-      const {limit, offset} = params;
+      const where: FindConditions<Product> = {};
+      const { limit, offset } = params;
+      const { maxPrice, minPrice } = params;
+      if (minPrice && maxPrice) {
+        where.price = Between(minPrice, maxPrice);
+      }
       return this.productRepo.find({
         relations: ['brand'],
+        where,
         take: limit,
         skip: offset,
       });
