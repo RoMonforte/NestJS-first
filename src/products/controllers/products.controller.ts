6f90b3b0a9067566fd9facf94df097a42e37278b
Rodrigo Monforte <rodrigomonforte94@gmail.com>
2023-01-16 21:31:30 -0600

    add roles control to endpoints

--- src/products/controllers/products.controller.ts
+++ src/products/controllers/products.controller.ts
@@ -21,9 +21,12 @@ import {
   FilterProductsDto,
 } from 'src/products/dtos/products.dto';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
 import { Public } from 'src/auth/decorators/public.decorator';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { Role } from 'src/auth/models/roles.model';
 
-@UseGuards(JwtAuthGuard)
+@UseGuards(JwtAuthGuard, RolesGuard)
 @ApiTags('products')
 @Controller('products')
 export class ProductsController {
@@ -45,6 +48,7 @@ export class ProductsController {
     return this.productsService.findOne(productId);
   }
 
+  @Roles(Role.ADMIN)
   @Post()
   create(@Body() payload: CreateProductDto) {
     return this.productsService.create(payload);
